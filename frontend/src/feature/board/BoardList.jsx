10e861fe5f18ca068320a1798351d1bec8de0e06
sebaek <sebaek@gmail.com>
2025-07-09 11:55:08 +0900

    게시물 목록 보기 작업중

--- frontend/src/feature/board/BoardList.jsx
+++ frontend/src/feature/board/BoardList.jsx
@@ -1,7 +1,26 @@
+import { Table } from "react-bootstrap";
+import { useEffect } from "react";
+import axios from "axios";
+
 export function BoardList() {
+  useEffect(() => {
+    // 마운트될때(initial render 시) 실행되는 코드
+    axios
+      .get("/api/board/list")
+      .then((res) => {
+        console.log("잘 될 때 코드");
+      })
+      .catch((err) => {
+        console.log("잘 안될 때 코드");
+      })
+      .finally(() => {
+        console.log("항상 실행 코드");
+      });
+  }, []);
   return (
     <div>
       <h3>글 목록</h3>
+      <Table></Table>
     </div>
   );
 }
