e5686102f963248842be593620277d739ae188d4
sebaek <sebaek@gmail.com>
2025-07-09 10:26:20 +0900

    글 작성 후 toast 띄우고 / 로 이동

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,14 @@
-import { BrowserRouter, Link, Outlet, Route, Routes } from "react-router";
+import {
+  BrowserRouter,
+  Link,
+  Outlet,
+  Route,
+  Routes,
+  useNavigate,
+} from "react-router";
 import { useState } from "react";
 import axios from "axios";
+import { toast } from "react-toastify";
 
 function MainLayout() {
   return (
@@ -30,6 +38,8 @@ function BoardAdd() {
   const [content, setContent] = useState("");
   const [author, setAuthor] = useState("");
 
+  const navigate = useNavigate();
+
   function handleSaveButtonClick() {
     axios
       .post("/api/board/add", {
@@ -38,7 +48,13 @@ function BoardAdd() {
         author: author,
       })
       .then((res) => {
-        console.log("잘 되면 실행되는 코드");
+        const message = res.data.message;
+        if (message) {
+          // toast 띄우기
+          toast(message.text, { type: message.type });
+        }
+        // "/"로 이동
+        navigate("/");
       })
       .catch((err) => {
         console.log("잘 안되면 실행되는 코드");
