c08fefe657eb06a80a03864738054249ed5d3f50
sebaek <sebaek@gmail.com>
2025-07-11 12:22:35 +0900

    토큰을 요청 시 가져가기

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,9 +17,35 @@ function App() {
     localStorage.removeItem("token");
   }
 
+  function handleButton4Click() {
+    // get,post,put,delete
+    // 토큰 안들고 가기
+    axios.get("/api/learn/jwt/sub2");
+  }
+
+  function handleButton3Click() {
+    // localStorage에서 token 얻기
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      // 있으면 토큰 들고 요청
+      // Authorization 헤더에 "Bearer "를 앞에 붙이고
+      axios.get("/api/learn/jwt/sub2", {
+        headers: {
+          Authorization: "Bearer " + token,
+        },
+      });
+    } else {
+      // 없으면 토큰 안들고 요청
+      axios.get("/api/learn/jwt/sub2");
+    }
+  }
+
   return (
     <div>
       <h3>jwt 로그인 연습</h3>
+      <button onClick={handleButton4Click}>4. 토큰 안들고 요청</button>
+      <button onClick={handleButton3Click}>3. 토큰 들고 요청</button>
       <button onClick={handleButton2Click}>2. token 지우기(logout)</button>
       <button onClick={handleButton1Click}>1. token 얻기 (login)</button>
     </div>
