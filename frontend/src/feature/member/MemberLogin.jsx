220c54f53046cee8c76bbb8cdc205c219832902c
sebaek <sebaek@gmail.com>
2025-07-11 15:13:18 +0900

    로그인 화면

--- frontend/src/feature/member/MemberLogin.jsx
+++ frontend/src/feature/member/MemberLogin.jsx
@@ -9,6 +9,7 @@ import {
 import { useState } from "react";
 import { useNavigate } from "react-router";
 import axios from "axios";
+import { toast } from "react-toastify";
 
 export function MemberLogin() {
   const [email, setEmail] = useState("");
@@ -18,8 +19,23 @@ export function MemberLogin() {
   function handleLogInButtonClick() {
     axios
       .post("/api/member/login", { email: email, password: password })
-      .then((res) => {})
-      .catch((err) => {})
+      .then((res) => {
+        const token = res.data.token;
+        localStorage.setItem("token", token);
+
+        const message = res.data.message;
+        if (message) {
+          toast(message.text, { type: message.type });
+        }
+
+        navigate("/");
+      })
+      .catch((err) => {
+        const message = err.response.data.message;
+        if (message) {
+          toast(message.text, { type: message.type });
+        }
+      })
       .finally(() => {});
   }
 
