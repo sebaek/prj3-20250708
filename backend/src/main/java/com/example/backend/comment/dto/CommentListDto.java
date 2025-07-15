86a326564d2cdb0fedce30f7b603b3cf4a0a7275
sebaek <sebaek@gmail.com>
2025-07-15 14:32:49 +0900

    수정/삭제 버튼 본인 것만 보이게

--- backend/src/main/java/com/example/backend/comment/dto/CommentListDto.java
+++ backend/src/main/java/com/example/backend/comment/dto/CommentListDto.java
@@ -14,6 +14,7 @@ import java.time.ZoneId;
 public class CommentListDto {
     private Integer id;
     private Integer boardId;
+    private String authorEmail;
     private String authorNickName;
     private String comment;
     private LocalDateTime insertedAt;
