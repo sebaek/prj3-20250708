8ab6a57df74486e3defe76382f4c0cd2e7db60b5
sebaek <sebaek@gmail.com>
2025-07-16 09:43:14 +0900

    하트 클릭시 좋아요 데이터 입력/삭제

--- backend/src/main/java/com/example/backend/like/service/LikeService.java
+++ backend/src/main/java/com/example/backend/like/service/LikeService.java
@@ -1,9 +1,13 @@
 package com.example.backend.like.service;
 
+import com.example.backend.board.entity.Board;
+import com.example.backend.board.repository.BoardRepository;
 import com.example.backend.like.dto.LikeForm;
 import com.example.backend.like.entity.BoardLike;
 import com.example.backend.like.entity.BoardLikeId;
 import com.example.backend.like.repository.BoardLikeRepository;
+import com.example.backend.member.entity.Member;
+import com.example.backend.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
@@ -15,6 +19,8 @@ import org.springframework.transaction.annotation.Transactional;
 public class LikeService {
 
     private final BoardLikeRepository boardLikeRepository;
+    private final BoardRepository boardRepository;
+    private final MemberRepository memberRepository;
 
     public void update(LikeForm likeForm, Authentication authentication) {
         // 로그인 안했으면 exception
@@ -31,6 +37,11 @@ public class LikeService {
         } else {
             // 없으면 insert
             BoardLike boardLikeEntity = new BoardLike();
+            Board board = boardRepository.findById(likeForm.getBoardId()).get();
+            Member member = memberRepository.findById(authentication.getName()).get();
+            boardLikeEntity.setBoard(board);
+            boardLikeEntity.setMember(member);
+
             BoardLikeId boardLikeId = new BoardLikeId();
             boardLikeId.setBoardId(likeForm.getBoardId());
             boardLikeId.setMemberEmail(authentication.getName());
