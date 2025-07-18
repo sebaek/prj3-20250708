a4095d7256040d56e46aca648d1bd3ea067ea55c
sebaek <sebaek@gmail.com>
2025-07-18 15:30:43 +0900

    수정

--- frontend/src/common/AppNavBar.jsx
+++ frontend/src/common/AppNavBar.jsx
@@ -39,7 +39,7 @@ export function AppNavBar() {
       <Navbar expand="lg" className="bg-body-tertiary">
         <Container>
           <Navbar.Brand to="/" as={Link}>
-            PRJ3
+            플젝3
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
